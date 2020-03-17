6a0f6172e4c8802405db9020311fc9f76da7a1b4
Marina <ivanovicmimi@gmail.com>
2020-03-17 16:36:11 +0100

    reading from csv

--- src/sensors/scripts/utils/reader.py
+++ src/sensors/scripts/utils/reader.py
@@ -1,18 +1,7 @@
 import numpy as np 
 import csv
 import time
-# from sensors.msg import RawMeasurement
-
-######### OBRISI OVO ###########
-class RawMeasurement():
-
-    def __init__(self, day, max_temp, min_temp, avg_temp):
-        self.day = day
-        self.max_temp = max_temp
-        self.min_temp = min_temp
-        self.avg_temp = avg_temp
-
-################################
+from sensors.msg import RawMeasurement
 
 class MeasurementReader():
 
@@ -21,7 +10,7 @@ class MeasurementReader():
         self.filename = filename if (filename is not None) else (
             'data/weather_data_nyc_centralpark_2016.csv'
         )
-        self.delimiter = ','
+        self.delimiter = delimiter
         self.file_size = file_size
         self.current_day = -1
         self._read_data()
@@ -32,31 +21,20 @@ class MeasurementReader():
         data_csv = csv.reader(data_file, delimiter=self.delimiter)
 
         # Read data
-        # TODO(jana): read all measurements in self.data
-
         data_iterator = iter(data_csv)
         next(data_iterator)
         self.data = []
         for row in data_iterator:
-            one_day_data = RawMeasurement(time.strptime(row[0],'%d-%m-%Y'), 
-            float(row[1]), float(row[2]), float(row[3]))
+            one_day_data = RawMeasurement(
+                row[0], float(row[3]), float(row[1]), float(row[2])
+            )
             self.data.append(one_day_data)
 
     def has_next_measurement(self):
-        # TODO(jana): return False when there are no more measurements
-
         if (self.current_day == (len(self.data) - 1)):
             return False
         return True
 
     def get_next_measurement(self):
-        # TODO(jana): get next measurement from self.data
-
         self.current_day += 1
         return self.data[self.current_day]
-
-if __name__ == '__main__':
-
-    reading = MeasurementReader()
-    for i in range(0,366):
-        print(reading.has_next_measurement())
