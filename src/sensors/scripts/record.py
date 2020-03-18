d23acf4494510260682c25cb9e00f088ada83720
Marina <ivanovicmimi@gmail.com>
2020-03-18 03:44:04 +0100

    recording connected

--- src/sensors/scripts/record.py
+++ src/sensors/scripts/record.py
@@ -6,28 +6,26 @@ from sensors.srv import RecordMeasurement, RecordMeasurementRequest, RecordMeasu
 from utils.analysis import process_measurement, prepare_to_record
 import csv
 
-
 def process_service_request(request):
     to_record = prepare_to_record(request.measurement)
-    response = RecordMeasurementResponse(True)
-    response = True
 
-    if (response):
-        new_records_file = open("../../../data/new_records.csv",'a+')
-        file_writer = csv.writer(new_records_file, delimiter = ',', lineterminator='\n')
-        file_writer.writerow([to_record.day, str(to_record.min_temp), 
+    new_records_file = open('data/new_records.csv','a+')
+    file_writer = csv.writer(new_records_file, delimiter = ',', lineterminator='\n')
+    file_writer.writerow([to_record.day, str(to_record.min_temp), 
         str(to_record.max_temp), str(to_record.avg_temp)])
-        new_records_file.close()
-        rospy.loginfo('New measurement recorded.')
-    return response
+    new_records_file.close()
+
+    rospy.loginfo('New measurement recorded.')
+    return RecordMeasurementResponse(True)
 
 def start_recording_service():
     rospy.init_node('recording', anonymous=False)
 
-    new_records_file = open("../../../data/new_records.csv",'a+')
-    file_writer = csv.writer(new_records_file, delimiter = ',', lineterminator='\n')
+    new_records_file = open('data/new_records.csv','a+')
+    file_writer = csv.writer(new_records_file, delimiter = ',',
+        lineterminator='\n')
     file_writer.writerow(['date', 'minimal temperature', 
-    'maximum temperature', 'average temperature'])
+        'maximum temperature', 'average temperature'])
     new_records_file.close()
 
     rospy.Service(
