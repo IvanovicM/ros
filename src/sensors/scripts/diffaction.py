735b9d73cbf9aea614ad924c6032a4090a9368e1
Marina <ivanovicmimi@gmail.com>
2020-03-18 16:53:30 +0100

    big diff action

--- src/sensors/scripts/diffaction.py
+++ src/sensors/scripts/diffaction.py
@@ -1,15 +1,34 @@
 #! /usr/bin/env python
 
+import actionlib
 import rospy
 from sensors.msg import ProcessedMeasurement
-from sensors.msg import DiffActionGoal, DiffActionResult, DiffActionFeedback
+from sensors.msg import BigTempDiffAction, BigTempDiffGoal, BigTempDiffResult, BigTempDiffFeedback
+
+class BigTempDiffActionServer:
+
+    def __init__(self):
+        self.server = actionlib.SimpleActionServer(
+            'big_temp_diff', BigTempDiffAction, self.execute, False
+        )
+        self.server.start()
+        self.diff_counter = 0
+
+    def execute(self, goal):
+        self.diff_counter += goal.inc_num
+        self.server.set_succeeded()
+        
+        rospy.loginfo(
+            'Action executed. Diff counter is: {}'.format(self.diff_counter)
+        )
 
 def start_diff_action():
-    rospy.init_node('diff_action', anonymous=False)
+    rospy.init_node('big_temp_diff_action_server')
+    server = BigTempDiffActionServer()
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
         start_diff_action()
     except rospy.ROSInterruptException:
         pass
-
--- src/sensors/scripts/processing.py
+++ src/sensors/scripts/processing.py
@@ -1,10 +1,18 @@
 #! /usr/bin/env python
 
+import actionlib
 import rospy
+from sensors.msg import BigTempDiffAction, BigTempDiffGoal, BigTempDiffResult, BigTempDiffFeedback
 from sensors.msg import ProcessedMeasurement
 from sensors.msg import RawMeasurement
 from sensors.srv import RecordMeasurement, RecordMeasurementRequest, RecordMeasurementResponse
 from utils.analysis import process_measurement
+
+def maybe_big_diff_action(measurement, action_client):
+    if measurement.big_difference:
+        rospy.loginfo('Big temp diff action sent.')
+        goal = BigTempDiffGoal(1)
+        action_client.send_goal(goal)
 
 def record_measurement(request):
     rospy.wait_for_service('record_data_service')
@@ -21,21 +29,26 @@ def record_measurement(request):
     except rospy.ServiceException, e:
         print('Service failed, {}'.format(e))
 
-def measurement_processing(measurement, publisher):
+def measurement_processing(measurement, args):
     proc_measurement = process_measurement(measurement)
-    publisher.publish(proc_measurement)
-    record_measurement(proc_measurement)
-
     rospy.loginfo('New measurement processed.')
+
+    args[0].publish(proc_measurement)
+    record_measurement(proc_measurement)
+    maybe_big_diff_action(proc_measurement, args[1])
 
 def start_measurement_processing():
     rospy.init_node('processing', anonymous=False)
     processed_data_publisher = rospy.Publisher(
         'processed_data_topic', ProcessedMeasurement, queue_size = 10
     )
+    action_client = actionlib.SimpleActionClient(
+        'big_temp_diff', BigTempDiffAction
+    )
+    action_client.wait_for_server()
     rospy.Subscriber(
         'raw_data_topic', RawMeasurement, measurement_processing,
-        (processed_data_publisher)
+        (processed_data_publisher, action_client)
     )
     rospy.spin()
 
