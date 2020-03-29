52d9c4c3dcaabaa2cc00883f9d301e3802d7d1c4
Jana Kovacevic <jana.kovacevic@sipodsoftware.com>
2020-03-29 19:55:13 +0200

    pls

--- src/kinematics/scripts/control/state.py
+++ src/kinematics/scripts/control/state.py
@@ -25,6 +25,10 @@ class State(object):
     def process_odometry(self, odometry):
         pass
 
+    def process_vw_update(self, v, w):
+        pass
+
+
 class ManualState(State):
 
     def __init__(self, command_publisher):
@@ -35,13 +39,16 @@ class ManualState(State):
         super(ManualState, self).print_message()
         print('To specify lin/ang velocity input (v w).')
 
+    def process_vw_update(self, v, w):
+        # TODO(jana): super().send_cmd(v, w) + dobar odnos v i w, kao iz pdf
+        super(ManualState, self).send_cmd(v, w)
+
 class AutoState(State):
 
     def __init__(self, command_publisher):
         super(AutoState, self).__init__(command_publisher)
         self.target_x = 0.0
         self.target_y = 0.0
-        self.v_wanted = 1
         self.k_p = 1
         self.k_a = 2
         self.k_b = -1
@@ -49,8 +56,7 @@ class AutoState(State):
     def print_message(self):
         print('\nAUTO MODE')
         super(AutoState, self).print_message()
-        print('To specify target input and speed (x y v).'
-              'Default speed value: 1m/s.') 
+        print('To specify target input and speed (x y v). Defolt speed value: 1m/s') 
 
     def set_target(self, x, y, v_wanted = 1):
         self.target_x = x 
@@ -59,15 +65,17 @@ class AutoState(State):
         print('New target is set.')
 
     def xyz2polar(self, odometry):
-        delta_x = self.target_x - odometry.position.x
+        delta_x = self.target_x - odometry.position.x #nisam sigurna da ovo moze ovako, iskreno se nadam da moze
         delta_y = self.target_y - odometry.position.y
         rho = sqrt(delta_x**2 + delta_y**2)
         theta = np.arctan2(odometry.position.x / odometry.position.y)
         alpha = np.arctan2(delta_y , delta_x) - theta
         beta = -theta - alpha
 
-        if alpha < -np.pi/2 or alpha > np.pi/2:
-            if alpha > np.pi/2:
+        # Ovo je bas bio ozbiljan freestyle 
+
+        if (alpha < -np.pi/2 or alpha > np.pi/2):
+            if(alpha > np.pi/2):
                 alpha = alpha - np.pi/3 - theta
                 beta = np.pi - beta
             else:
@@ -79,11 +87,15 @@ class AutoState(State):
         return (rho, alpha, beta)
 
     def process_odometry(self, odometry):
+        # TODO(jana): kontroler, na osnovu odometry odrediti v i w pa
+        # send_cmd(v, w) + dobar odnos v i w, kao iz pdf
         rho, alpha, beta = xyz2polar(odometry)
         v = self.k_p * rho
         w = self.k_a * alpha + self.k_b * beta
         speed_scaling_const = v / w
         w = self.v_wanted / speed_scaling_const
+
+
 
 class ExitState(State):
 
