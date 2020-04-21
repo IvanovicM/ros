854d4e336c7f1016f70431a3f69800ef373864fd
Marina <ivanovicmimi@gmail.com>
2020-04-21 10:15:33 +0200

    delete not needed spaces

--- src/kalman/scripts/kfilter/kfilter.py
+++ src/kalman/scripts/kfilter/kfilter.py
@@ -3,11 +3,9 @@ import threading
 
 def synchronized(func):
     func.__lock__ = threading.Lock()
-		
     def synced_func(*args, **kws):
         with func.__lock__:
             return func(*args, **kws)
-
     return synced_func
 
 class KalmanFilter():
@@ -24,20 +22,16 @@ class KalmanFilter():
         for i in range(len(joint_states.name)):
             if joint_states.name[i] == 'wheel_left_joint':
                 self.ds_r = (
-                    0.0
-                    if self.s_r is None
+                    0.0 if self.s_r is None
                     else joint_states.position[i] - self.s_r
                 )
                 self.s_r = joint_states.position[i]
             if joint_states.name[i] == 'wheel_right_joint':
                 self.ds_l = (
-                    0.0
-                    if self.s_l is None
+                    0.0 if self.s_l is None
                     else joint_states.position[i] - self.s_l
                 )
                 self.s_l = joint_states.position[i]
-
-        print('ds_l = {:.2f}, ds_r = {:.2f}'.format(self.ds_l, self.ds_r))
 
     @synchronized
     def save_line_segments(self, line_segments):
--- src/kinematics/scripts/control/controller.py
+++ src/kinematics/scripts/control/controller.py
@@ -8,11 +8,9 @@ from state import ManualState, AutoState, ExitState
 	
 def synchronized(func):
     func.__lock__ = threading.Lock()
-		
     def synced_func(*args, **kws):
         with func.__lock__:
             return func(*args, **kws)
-
     return synced_func
 
 class StateController():
