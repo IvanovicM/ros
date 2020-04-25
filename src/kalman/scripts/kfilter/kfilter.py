0f76a973a33052de2dc24a4605d991ac9c8d8493
Marina <ivanovicmimi@gmail.com>
2020-04-25 18:58:09 +0200

    synchronized

--- src/kalman/scripts/kfilter/kfilter.py
+++ src/kalman/scripts/kfilter/kfilter.py
@@ -5,12 +5,19 @@ from gmap import GlobalMap
 from gposition import GlobalPosition
 from math import cos, sin
 
-def synchronized(func):
-    func.__lock__ = threading.Lock()
-    def synced_func(*args, **kws):
-        with func.__lock__:
-            return func(*args, **kws)
-    return synced_func
+def synchronized(method):    
+    outer_lock = threading.Lock()
+    lock_name = "__"+method.__name__+"_lock"+"__"
+    
+    def sync_method(self, *args, **kws):
+        with outer_lock:
+            if not hasattr(self, lock_name):
+                setattr(self, lock_name, threading.Lock())
+            lock = getattr(self, lock_name)
+            with lock:
+                return method(self, *args, **kws)  
+
+    return sync_method
 
 class KalmanFilter():
 
--- src/kinematics/scripts/control/controller.py
+++ src/kinematics/scripts/control/controller.py
@@ -6,12 +6,19 @@ import rospy
 import threading
 from state import ManualState, AutoState, ExitState
 	
-def synchronized(func):
-    func.__lock__ = threading.Lock()
-    def synced_func(*args, **kws):
-        with func.__lock__:
-            return func(*args, **kws)
-    return synced_func
+def synchronized(method):    
+    outer_lock = threading.Lock()
+    lock_name = "__"+method.__name__+"_lock"+"__"
+    
+    def sync_method(self, *args, **kws):
+        with outer_lock:
+            if not hasattr(self, lock_name):
+                setattr(self, lock_name, threading.Lock())
+            lock = getattr(self, lock_name)
+            with lock:
+                return method(self, *args, **kws)  
+
+    return sync_method
 
 class StateController():
 
