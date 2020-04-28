4fd9f9dc38cf24cf95a3b98a319b2574d2fd07de
Marina <ivanovicmimi@gmail.com>
2020-04-28 18:08:41 +0200

    const tuned, kalman is running in a while

--- src/kalman/scripts/kalman_localization.py
+++ src/kalman/scripts/kalman_localization.py
@@ -8,31 +8,19 @@ from kfilter.visualize import RvizMarkerPublisher
 from sensor_msgs.msg import JointState
 from visualization_msgs.msg import MarkerArray
 
-joint_states_counter = -1
-line_segments_counter = -1
-detection_period = 10
-
-def collect_joint_states(joint_states, args):
-    global joint_states_counter, detection_period
-    joint_states_counter = (joint_states_counter + 1) % detection_period
-    if joint_states_counter != 0:
-        return
-
-    kalman_filter = args[0]
-    marker_publisher = args[1]
-
+def collect_joint_states(joint_states, kalman_filter):
     kalman_filter.save_joint_states(joint_states)
-    estimated_position = kalman_filter.filter()
-
-    marker_publisher.show_estimated_position(estimated_position)
-    marker_publisher.show_global_map(kalman_filter.global_map)
 
 def collect_line_segments(line_segments, kalman_filter):
-    global line_segments_counter, detection_period
-    line_segments_counter = (line_segments_counter + 1) % detection_period
-    if line_segments_counter != 0:
-        return
     kalman_filter.save_line_segments(line_segments)
+
+def filter_with_kalman(kalman_filter, marker_publisher):
+    while True:
+        estimated_position = kalman_filter.filter()
+        marker_publisher.show_estimated_position(estimated_position)
+        marker_publisher.show_global_map(kalman_filter.global_map)
+
+        rospy.sleep(0.1)
 
 def start_kalman_localization():
     rospy.init_node('kalman_localization', anonymous=False)
@@ -43,15 +31,14 @@ def start_kalman_localization():
     kalman_filter = KalmanFilter()
     marker_publisher = RvizMarkerPublisher(marker_topic_publisher)
     rospy.Subscriber(
-        'joint_states', JointState, collect_joint_states, 
-        (kalman_filter, marker_publisher)
+        'joint_states', JointState, collect_joint_states, kalman_filter
     )
     rospy.Subscriber(
         'line_segments', LineSegmentList, collect_line_segments, kalman_filter
     )
 
     rospy.loginfo('Kalman localization node is available.')
-    rospy.spin()
+    filter_with_kalman(kalman_filter, marker_publisher)
 
 if __name__ == '__main__':
     try:
--- src/kalman/scripts/kfilter/kfilter.py
+++ src/kalman/scripts/kfilter/kfilter.py
@@ -21,22 +21,26 @@ class KalmanFilter():
 
         self.pos = GlobalPosition()
         self.global_map = GlobalMap()
-        self.s_r = None
+        self.s_r_new = None
+        self.s_r_old = None
         self.ds_r = None
-        self.s_l = None
+        self.s_l_new = None
+        self.s_l_old = None
         self.ds_l = None
         self.line_segments = None
 
         self.b = 0.230 # 230 mm
         self.wheel_r = 0.035 # 35 mm
 
-        self.k_r = 0.1
-        self.k_l = 0.1
-        self.P = np.ones(shape=(3,3))
-        self.g = 10
+        self.k_r = 0.001
+        self.k_l = 0.001
+        self.P = 0.01 * np.ones(shape=(3,3))
+        self.g = 100
 
     def filter(self):
         self.mutex.acquire()
+        self._set_ds()
+
         pos_pred, P_pred = self._predict_position()
 
         # Any measurement?
@@ -74,15 +78,15 @@ class KalmanFilter():
             [0, 0, 1],
         ])
         F_u = np.array([
-            [(1/2 * cos(self.pos.theta + dtheta/2) - 
+            [(1/2 * cos(self.pos.theta + dtheta/2) + 
               ds/(2*self.b) * sin(self.pos.theta + dtheta/2)),
-              (1/2 * cos(self.pos.theta + dtheta/2) + 
+              (1/2 * cos(self.pos.theta + dtheta/2) - 
               ds/(2*self.b) * sin(self.pos.theta + dtheta/2))],
-            [(1/2 * sin(self.pos.theta + dtheta/2) + 
+            [(1/2 * sin(self.pos.theta + dtheta/2) - 
               ds/(2*self.b) * cos(self.pos.theta + dtheta/2)),
-              (1/2 * sin(self.pos.theta + dtheta/2) - 
+              (1/2 * sin(self.pos.theta + dtheta/2) + 
               ds/(2*self.b) * cos(self.pos.theta + dtheta/2))],
-            [1 / self.b, -1 / self.b],
+            [-1 / self.b, 1 / self.b],
         ])
         P_pred = (
             np.matmul(np.matmul(F_x, self.P), F_x.T) +
@@ -137,7 +141,7 @@ class KalmanFilter():
     def _filter_position(self, pos_pred, P_pred, H, R, v):
         if R is None or len(R) == 0 or len(R) != len(H) or len(R) != len(v):
             self.P = P_pred
-            self.pos = pos_pred
+            self.pos.set_position(pos_pred.x, pos_pred.y, pos_pred.theta)
             return
 
         # Block diagonal R
@@ -160,25 +164,31 @@ class KalmanFilter():
         pos_inovation = np.matmul(self.K, v)
 
         self.pos.set_position(
-            x=self.pos.x + pos_inovation.T[0][0],
-            y=self.pos.y + pos_inovation.T[0][1],
-            theta=self.pos.theta + pos_inovation.T[0][2]
+            x=pos_pred.x + pos_inovation.T[0][0],
+            y=pos_pred.y + pos_inovation.T[0][1],
+            theta=scale_angle(pos_pred.theta)
         )
 
+    def _set_ds(self):
+        self.ds_r = (
+            0.0 if self.s_r_old is None or self.s_r_new is None
+            else self.s_r_new - self.s_r_old
+        )
+        self.s_r_old = self.s_r_new
+        self.ds_l = (
+            0.0 if self.s_l_old is None or self.s_l_new is None
+            else self.s_l_new - self.s_l_old
+        )
+        self.s_l_old = self.s_l_new
+
     def save_joint_states(self, joint_states):
+        self.mutex.acquire()
         for i in range(len(joint_states.name)):
             if joint_states.name[i] == 'wheel_right_joint':
-                self.ds_r = (
-                    0.0 if self.s_r is None
-                    else joint_states.position[i] * self.wheel_r - self.s_r
-                )
-                self.s_r = joint_states.position[i] * self.wheel_r
+                self.s_r_new = joint_states.position[i] * self.wheel_r
             if joint_states.name[i] == 'wheel_left_joint':
-                self.ds_l = (
-                    0.0 if self.s_l is None
-                    else joint_states.position[i] * self.wheel_r - self.s_l
-                )
-                self.s_l = joint_states.position[i] * self.wheel_r
+                self.s_l_new = joint_states.position[i] * self.wheel_r
+        self.mutex.release()
 
     def save_line_segments(self, line_segments):
         self.mutex.acquire()
